576624c31704bee6c985872ff17eebb6a2b3e841
Marc André Haley <hmarcand@ualberta.ca>
2022-02-28 18:41:14 -0700

    added map in map fragment

--- app/src/main/java/com/example/myapplication/ui/map/MapFragment.java
+++ app/src/main/java/com/example/myapplication/ui/map/MapFragment.java
@@ -1,10 +1,20 @@
 package com.example.myapplication.ui.map;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,18 +25,39 @@ import com.example.myapplication.databinding.FragmentMapBinding;
 public class MapFragment extends Fragment {
 
     private FragmentMapBinding binding;
+    private MapView mMapView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Context ctx = getActivity().getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        MapViewModel mapViewModel =
-                new ViewModelProvider(this).get(MapViewModel.class);
+        //final Context context = this.getActivity();
 
-        binding = FragmentMapBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        //MapViewModel mapViewModel =
+                //new ViewModelProvider(this).get(MapViewModel.class);
 
-        final TextView textView = binding.textMap;
-        mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        //binding = FragmentMapBinding.inflate(inflater, container, false);
+        //View root = binding.getRoot();
+
+        mMapView = new MapView(inflater.getContext());
+        mMapView.setDestroyMode(false);
+
+        IMapController mapController = mMapView.getController();
+        mapController.setZoom(12);
+        GeoPoint startPoint = new GeoPoint(53.5461, -113.4938);
+        mapController.setCenter(startPoint);
+
+        //mMapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+
+        //final TextView textView = binding.textMap;
+        //mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return mMapView;
     }
 
     @Override
