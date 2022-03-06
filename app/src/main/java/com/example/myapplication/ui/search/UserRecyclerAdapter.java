2d606324255f0ead4684ecfb29e744ffab7fa5de
ervin <ervinbinujoseph@gmail.com>
2022-03-05 20:18:34 -0700

    Figured out github, yay

---
+++ app/src/main/java/com/example/myapplication/ui/search/UserRecyclerAdapter.java
@@ -0,0 +1,88 @@
+package com.example.myapplication.ui.search;
+
+import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.Context;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.dataClasses.user.Player;
+
+import java.util.ArrayList;
+
+public class UserRecyclerAdapter extends RecyclerView.Adapter<UserRecyclerAdapter.ViewHolder> {
+
+    private ArrayList<Player> users;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    UserRecyclerAdapter(Context context, ArrayList<Player> users) {
+        this.mInflater = LayoutInflater.from(context);
+        this.users = users;
+    }
+
+    // inflates the row layout from xml when needed
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.search_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Player player = users.get(position);
+        holder.username.setText(player.getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        ImageView profilePic;
+        TextView username;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            profilePic = itemView.findViewById(R.id.profilepic);
+            username = itemView.findViewById(R.id.username);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+
+    // convenience method for getting data at click position
+    Player getItem(int id) {
+        return users.get(id);
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(UserRecyclerAdapter.ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
