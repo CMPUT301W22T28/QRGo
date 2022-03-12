9adcf15f9413264b1cb2e4aeb586470d6509e0a8
smallvalthoss <48263101+smallvalthoss@users.noreply.github.com>
2022-03-12 14:30:14 -0700

    added tests for ScoringQrCode

---
+++ app/src/test/java/com/example/myapplication/ScoringQrCodeTest.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import com.example.myapplication.dataClasses.qrCode.ScoringQRCode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScoringQrCodeTest {
+    private ScoringQRCode qrCode;
+
+
+    @Before
+    public void setup() {
+        qrCode = new ScoringQRCode("hash");
+    }
+
+    @Test
+    public void scoreTest() {
+
+    }
+
+}
