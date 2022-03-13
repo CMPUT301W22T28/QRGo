683be8dc1854c59a04b998de3263e5697a9ed9f8
smallvalthoss <48263101+smallvalthoss@users.noreply.github.com>
2022-03-12 18:35:40 -0700

    To pull from main

--- app/src/androidTest/java/com/example/myapplication/LoginActivityUITest.java
+++ app/src/androidTest/java/com/example/myapplication/LoginActivityUITest.java
@@ -4,6 +4,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -14,12 +15,23 @@ import com.robotium.solo.Solo;
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityUITest {
     private Solo solo;
+    private final String testingUsername = "usernameForIntentTesting";
 
     @Rule
-    public ActivityTestRule<LoginActivity> activityActivityTestRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+    public ActivityTestRule<LoginActivity> rule = new ActivityTestRule<>(LoginActivity.class);
 
     @Before
     public void setUp() throws Exception {
-//        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    @Test
+    public void start() {
+        rule.getActivity();
+    }
+
+    @Test
+    public void checkProperLogin() {
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
     }
 }
