08d5efce1c18ada1dbcd1c6fc9879e46a2ff999f
Marc André Haley <hmarcand@ualberta.ca>
2022-03-14 11:45:40 -0600

    added test dependencies

--- app/src/androidTest/java/com/example/myapplication/MapFragmentUITest.java
+++ app/src/androidTest/java/com/example/myapplication/MapFragmentUITest.java
@@ -1,11 +1,64 @@
 package com.example.myapplication;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.content.Intent;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.myapplication.activity.MainActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
 /**
- * Tests UI in MapFragment
+ * Tests UI in MapFragment. Espresso test framework is used
  *
  * @author CMPUT 301 Team 28, Marc-Andre Haley
  *
+ * March 12, 2022
+ *
  */
+@RunWith(AndroidJUnit4.class)
 public class MapFragmentUITest {
+
+    private final String testUsername = "marc";
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class){
+        @Override
+        protected Intent getActivityIntent() {
+            Intent intent = new Intent(InstrumentationRegistry.getInstrumentation().getTargetContext(), MainActivity.class);
+            intent.putExtra("Username", testUsername);
+            return intent;
+        }
+    };
+
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void init() {
+        mainActivityActivityTestRule.getActivity().getSupportFragmentManager().beginTransaction();
+    }
+
+
+    @Test
+    public void testProfileShowsUp() {
+        onView(withText(testUsername)).check(matches(isDisplayed()));
+    }
 
 }
