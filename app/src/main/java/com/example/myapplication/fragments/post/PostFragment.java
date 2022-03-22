92962ce61ec4809b0f4fdf0c05a95c52c0d4db36
Marc André Haley <hmarcand@ualberta.ca>
2022-03-22 15:04:47 -0600

    added tab listeners

--- app/src/main/java/com/example/myapplication/fragments/post/PostFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/post/PostFragment.java
@@ -8,18 +8,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentPostBinding;
+import com.google.android.material.tabs.TabLayout;
 
 public class PostFragment extends Fragment {
 
     private PostViewModel postViewModel;
 
     private FragmentPostBinding binding;
+
+    TabLayout tabLayout;
 
     public static PostFragment newInstance() {
         return new PostFragment();
@@ -41,7 +45,33 @@ public class PostFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // stuff for view model for this fragment
+        tabLayout = binding.postTabLayout;
 
+        // start post info as default fragment
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getText().equals("Post")) {
+                    // display post info fragment
+                }
+                if (tab.getText().equals("Comments")) {
+                    // display comments fragment
+                }
+                if (tab.getText().equals("Scanned by")) {
+                    // display users that also scanned
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
